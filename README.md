697bd72fc3bb137f03912c8112a45d5bb78bda1f
pantwisnr <psnr562@gmail.com>
2023-02-28 12:39:15 -0800

    # week3 Assignment on Arrays and Functions

---
+++ README.md
@@ -0,0 +1,6 @@
+# Arrays and Functions
+
+
+## iterating and accessing elements of an array 
+
+## writing functions to solve problems
---
+++ arraysFunctions.js
@@ -0,0 +1,9 @@
+//  QUES 1 
+
+let ages = [ 3, 9, 23, 64, 2, 8, 28, 93];
+
+//  QUES 1A.
+//  QUES 1
+//  QUES 1
+//  QUES 1
+//  QUES 1
